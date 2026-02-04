5b93816d7f27b06dfd57097c50912faec7940191
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-02-04 03:17:41 +0000

    logo dan text jadikan hitam (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center" aria-label="PKH Mateng Homepage">
-      <span className="text-xl font-bold text-emerald-800 font-headline">PKH Mateng</span>
+      <span className="text-xl font-bold text-foreground font-headline">PKH Mateng</span>
     </Link>
   );
 }
